Added analytic gradients and fixed the number of grid points

RESP/dotsphere.py
@@ -316,5 +316,11 @@ def dotsphere(density):
         vertices = dotsphere1(density)
     else:
         vertices = dotsphere2(density)
-    vertices = refine_dotsphere(vertices)
+    keep = np.zeros(density, dtype=int)
+    if density < len(vertices):
+        import random
+        random.seed(0)
+        for i in range(len(keep)):
+            keep[i] = random.randint(0, len(vertices)-1)
+    vertices = refine_dotsphere(vertices[keep])
     return vertices

RESP/resp.py
@@ -72,14 +72,14 @@ def resp(wfn, options={}):
     options['invr'] = 1.0/scipy.spatial.distance_matrix(points, coordinates)
     options['invr'] *= bohr_to_angstrom
     # Run the optimization using SciPy
-    charges = scipy.optimize.minimize(resp_objective, options['charges'], args=(options), method='SLSQP', tol=1e-8, options={'ftol':1e-8},
+    charges = scipy.optimize.minimize(resp_objective, options['charges'], args=(options), jac=True, method='SLSQP', tol=1e-8, options={'ftol':1e-8},
               constraints={'type': 'eq', 'fun':resp_constraint, 'args':[options['mol_charge'], options['CHARGE_GROUPS'], False]})
     charges = charges.x
     if options['TWO_STAGE_FIT']:
         a = options['RESP_A']
         options['RESP_A'] = options['RESP_A2']
         options['charges'] = charges
-        charges2 = scipy.optimize.minimize(resp_objective, charges[options['FIT2']], args=(options, True), method='SLSQP', tol=1e-8, options={'ftol':1e-8},
+        charges2 = scipy.optimize.minimize(resp_objective, charges[options['FIT2']], args=(options, True), jac=True, method='SLSQP', tol=1e-8, options={'ftol':1e-8},
                    constraints={'type': 'eq', 'fun':resp_constraint, 'args':[options['mol_charge'], options['CHARGE_GROUPS2'], True, options['charges'], options['FIT2']]})
         charges[options['FIT2']] = charges2.x
         options['RESP_A'] = a

RESP/respfit.py
@@ -22,7 +22,19 @@ def resp_objective(charges, options, two_stage=False):
     for charge in charges:
         chi2_rstr += np.sqrt(charge**2  + options['RESP_B']**2) - options['RESP_B']
     chi2_rstr *= options['RESP_A']
-    return chi2_esp + chi2_rstr
+    chi2_d_esp = []
+    for i in range(len(charges)):
+        grad = 0
+        for j in range(len(options['esp_values'])):
+            grad += (options['esp_values'] - expected_esp)[j]*options['invr'][j][i]
+        grad *= -2
+        chi2_d_esp.append(grad)
+    chi2_d_rstr = []
+    for i in range(len(charges)):
+        chi2_d_rstr.append(options['RESP_A']*charges[i]/np.sqrt(charges[i]**2+options['RESP_B']**2))
+    chi2_d_esp = np.array(chi2_d_esp)
+    chi2_d_rstr = np.array(chi2_d_rstr)
+    return (chi2_esp + chi2_rstr, chi2_d_esp + chi2_d_rstr)
 
 def resp_constraint(charges, mol_charge, charge_groups=None, two_stage=False, all_charges=None, fit2=None):
     total_charge = 0